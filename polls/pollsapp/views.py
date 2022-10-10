a83d492a3cb258073ed9cbcd049dbcf8e6201afc
Nikita Petrykin <nvnedu2120@gmail.com>
2022-10-10 13:47:42 +0200

    feat: basic project setup (routes & views)
    Necessary views and routes created

--- polls/pollsapp/views.py
+++ polls/pollsapp/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse("This is main page")
+
+def detail(request, question_id):
+    return HttpResponse("Question details will be displayed here #%s" % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You can vote on this page #%s" % question_id)
+
+def result(request, question_id):
+    return HttpResponse("Poll results will be displayed here #%s" % question_id)
