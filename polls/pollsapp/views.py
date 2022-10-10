cee86d82b4f5391f99246ad58bd96f4481d98603
Nikita Petrykin <nvnedu2120@gmail.com>
2022-10-10 16:31:44 +0200

    feat: HTML pages & some views
    Created template and required HTML pages
    Added views for index and detail routes

--- polls/pollsapp/views.py
+++ polls/pollsapp/views.py
@@ -1,16 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Question
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("This is main page")
+    latest_question_list = Question.objects.order_by('-date')[:5]
+
+    return render(request, "pollsapp/index.html", {
+        "latest_question_list": latest_question_list
+    })
+
 
 def detail(request, question_id):
-    return HttpResponse("Question details will be displayed here #%s" % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+
+    return render(request, "pollsapp/detail.html", {
+        "question": question
+    })
+
 
 def vote(request, question_id):
     return HttpResponse("You can vote on this page #%s" % question_id)
+
 
 def result(request, question_id):
     return HttpResponse("Poll results will be displayed here #%s" % question_id)
