1a9701a883dd3bc225cc566828e2b29936b4936f
Nikita Petrykin <nvnedu2120@gmail.com>
2022-10-10 14:51:42 +0200

    feat: created superuser & admin site

--- polls/pollsapp/admin.py
+++ polls/pollsapp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Choice, Question
 
 # Register your models here.
+
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ("question_id", "choice", "votes")
+
+
+admin.site.register(Question)
+admin.site.register(Choice, ChoiceAdmin)
