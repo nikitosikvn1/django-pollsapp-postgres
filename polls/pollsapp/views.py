5e18d1f8a084b76895082b612798da8eb011eefb
Nikita Petrykin <nvnedu2120@gmail.com>
2022-10-29 15:27:31 +0200

    feat: some styles & bootstrap
    Prettier display of index.html
    Some styles for all pages

--- polls/pollsapp/views.py
+++ polls/pollsapp/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+from django.db.models import Sum
 from .models import Question, Choice
 
 # Create your views here.
 
 def index(request):
     latest_question_list = Question.objects.order_by('-date')[:5]
+    votes_num = [num.choices.aggregate(Sum('votes'))['votes__sum'] for num in latest_question_list]
 
+    questions_votes = zip(latest_question_list, votes_num)
     return render(request, "pollsapp/index.html", {
-        "latest_question_list": latest_question_list
+        "questions_votes": questions_votes
     })
 
 
