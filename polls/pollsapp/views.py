676b8cf5355094133182c76e1fa5396f52d9d6d9
Nikita Petrykin <nvnedu2120@gmail.com>
2022-10-29 12:00:04 +0200

    feat: vote and result views
    Added ability to vote and view results

--- polls/pollsapp/views.py
+++ polls/pollsapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-from .models import Question
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from .models import Question, Choice
 
 # Create your views here.
 
@@ -21,8 +22,23 @@ def detail(request, question_id):
 
 
 def vote(request, question_id):
-    return HttpResponse("You can vote on this page #%s" % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choices.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, "pollsapp/detail.html", {
+            "question": question,
+            "error_message": "You didn't select a choice."
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+
+        return HttpResponseRedirect(reverse('pollsapp:result', args=(question.id,)))
 
 
 def result(request, question_id):
-    return HttpResponse("Poll results will be displayed here #%s" % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "pollsapp/result.html", {
+        "question": question
+    })
