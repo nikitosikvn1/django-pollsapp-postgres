1063590649bccb2283c47e41edc1f97498bc8e82
Nikita Petrykin <nvnedu2120@gmail.com>
2022-10-29 15:54:33 +0200

    feat:  Basic search implemented
    Added search for questions by words and date

--- polls/pollsapp/views.py
+++ polls/pollsapp/views.py
@@ -1,18 +1,27 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
-from django.db.models import Sum
+from django.db.models import Sum, Q
 from .models import Question, Choice
 
 # Create your views here.
 
 def index(request):
-    latest_question_list = Question.objects.order_by('-date')[:5]
+    search_query = request.GET.get("search")
+    
+    if search_query:
+        latest_question_list = Question.objects.filter(Q(question__icontains=search_query) | Q(date__icontains=search_query))
+        header = f"Search results for: '{search_query}'"
+    else:
+        latest_question_list = Question.objects.order_by('-date')[:5]
+        header = "Recent questions"
+
     votes_num = [num.choices.aggregate(Sum('votes'))['votes__sum'] for num in latest_question_list]
 
     questions_votes = zip(latest_question_list, votes_num)
     return render(request, "pollsapp/index.html", {
-        "questions_votes": questions_votes
+        "questions_votes": questions_votes,
+        "header": header
     })
 
 
